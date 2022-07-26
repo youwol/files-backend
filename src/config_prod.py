b0654f2b3eb6f8f8d8d86d4da2e91984a2560008
Julien DÉCHARNE <jdecharne@youwol.com>
2022-07-26 18:01:46 +0200

    🎨 use constants for environment vars names

--- src/config_prod.py
+++ src/config_prod.py
@@ -7,28 +7,21 @@ from youwol_utils.clients.file_system.minio_file_system import MinioFileSystem
 from youwol_utils.clients.oidc.oidc_config import OidcInfos, PrivateClient
 from youwol_utils.context import DeployedContextReporter
 from youwol_utils.middlewares import AuthMiddleware
+from youwol_utils.servers.env import OPENID_CLIENT, MINIO, Env
 from youwol_utils.servers.fast_api import AppConfiguration, ServerOptions, FastApiMiddleware
 
 
 async def get_configuration() -> AppConfiguration[ServiceConfiguration[MinioFileSystem]]:
-    required_env_vars = [
-        "OPENID_BASE_URL",
-        "OPENID_CLIENT_ID",
-        "OPENID_CLIENT_SECRET",
-        "MINIO_HOST_PORT",
-        "MINIO_ACCESS_KEY",
-        "MINIO_ACCESS_SECRET",
-    ]
-
+    required_env_vars = OPENID_CLIENT + MINIO
     not_founds = [v for v in required_env_vars if not os.getenv(v)]
     if not_founds:
         raise RuntimeError(f"Missing environments variable: {not_founds}")
 
     openid_infos = OidcInfos(
-        base_uri=os.getenv("OPENID_BASE_URL"),
+        base_uri=os.getenv(Env.OPENID_BASE_URL),
         client=PrivateClient(
-            client_id=os.getenv("OPENID_CLIENT_ID"),
-            client_secret=os.getenv("OPENID_CLIENT_SECRET")
+            client_id=os.getenv(Env.OPENID_CLIENT_ID),
+            client_secret=os.getenv(Env.OPENID_CLIENT_SECRET)
         )
     )
 
@@ -36,9 +29,9 @@ async def get_configuration() -> AppConfiguration[ServiceConfiguration[MinioFile
         file_system=MinioFileSystem(
             bucket_name=Constants.namespace,
             client=Minio(
-                endpoint=os.getenv("MINIO_HOST_PORT"),
-                access_key=os.getenv("MINIO_ACCESS_KEY"),
-                secret_key=os.getenv("MINIO_ACCESS_SECRET"),
+                endpoint=f"{os.getenv(Env.MINIO_HOST)}:9000",
+                access_key=os.getenv(Env.MINIO_ACCESS_KEY),
+                secret_key=os.getenv(Env.MINIO_ACCESS_SECRET),
                 secure=False
             )
         )
